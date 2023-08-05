594c006e29810d12ce05c93bc89c9472281f66fe
Oleg <olegkorinblit@gmail.com>
2023-08-05 23:06:34 +0300

    Initial commit

---
+++ src/main/java/screens/AuthenticationScreen.java
@@ -0,0 +1,26 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AuthenticationScreen extends BaseScreen{
+
+
+    public AuthenticationScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//*[@resource-id='com.sheygam.contactapp:id/action_bar']/android.widget.TextView")
+    MobileElement activityViewText;
+    @FindBy(xpath="//*[@resource-id='com.sheygam.contactapp:id/inputEmail']")
+    MobileElement EditEmailInput;
+    @FindBy(xpath="//*[@resource-id='com.sheygam.contactapp:id/inputPassword']")
+    MobileElement EditPasswordInput;
+
+    @FindBy(xpath="//*[@resource-id='com.sheygam.contactapp:id/regBtn']")
+    MobileElement RegBtn;
+
+    @FindBy(xpath="//*[@resource-id='com.sheygam.contactapp:id/loginBtn']")
+    MobileElement LoginBtn;
+}
